f18078e29cf35f8f5b07ec906a6ffaa96248eec9
murmon <vivchar.danylo@gmail.com>
2014-01-13 18:46:01 +0200

    f2

--- models/Composer.php
+++ models/Composer.php
@@ -1,5 +1,6 @@
 <?php
 require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'php-activerecord' . DIRECTORY_SEPARATOR . 'ActiveRecord.php');
+require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config.php');
 /**
  *
  * @property int $id
