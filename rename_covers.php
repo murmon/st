5b0bbc3be799c32482fc4324c2a070301da7f822
murmon <vivchar.danylo@gmail.com>
2014-01-14 15:17:29 +0200

    f2

--- rename_covers.php
+++ rename_covers.php
@@ -12,21 +12,22 @@
 require 'models'.DIRECTORY_SEPARATOR.'Track.php';
 require 'Utf8.php';
 
-$dirs = array_filter(glob(Track::$search_tracks_in . '*'), 'is_dir');
+$covers = array_filter(glob('e:\projects\shopify\covers\\' . '*.jpg'), 'is_file');
+$failed = array();
 
-foreach ($dirs as $dir) {
-    $dir_basename = basename($dir);
-    $dir_ascii = \Patchwork\Utf8::toAscii($dir_basename);
+foreach ($covers as $cover) {
+    $cover_ascii = \Patchwork\Utf8::toAscii($cover);
 
-    echo $dir . " => $dir_ascii <br>";
-
-    if(rename($dir, $dir_ascii)){
-        echo $dir . " => $dir_ascii <br>";
+    if(rename($cover, $cover_ascii)){
+        echo $cover . " => $cover_ascii <br>";
     } else {
-        echo $dir . " RENAME FAILED <br>";
+        echo $cover . " RENAME FAILED <br>";
+        $failed[] = $cover;
     }
-
 }
+
+echo "=========================<br>";
+echo "FAILED" . count($failed) ." <br>";
 
 function dirSize($directory) {
     $size = 0;
@@ -36,25 +37,6 @@ function dirSize($directory) {
     return $size;
 }
 die();
-
-Config::getConfig();
-
-$tracks = Track::find('all'/*, array('conditions' => 'id = 1')*/);
-
-$not_found_files = 1;
-
-foreach ($tracks as $track) {
-    $full_track_path = Track::$search_tracks_in . stripSpaces($track->composer->name) . DIRECTORY_SEPARATOR . stripSpaces($track->name_ascii) . '.mp3';
-
-    if(!is_file($full_track_path)){
-        echo "$full_track_path<br>";
-        $not_found_files++;
-    }
-//    $track->name_ascii = trim($track->name_ascii);
-//    $track->save();
-}
-
-echo "Missing $not_found_files files";
 
 function humanFileSize($size, $unit = "")
 {
